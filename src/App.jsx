281531e6a1681c8e9c1de9da140cf90d8e337305
Melrizzuto <melrizzuto4@gmail.com>
2025-02-12 22:32:08 +0100

    implemented pagedetails.jsx and css module / updated general css index css /

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DefaultLayout from './layout/DefaultLayout';
 import Homepage from './pages/Homepage';
+import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         {/* Wrap the homepage inside Layout */}
         <Route path="/" element={<DefaultLayout />}>
           <Route index element={<Homepage />} />
+          <Route path="properties/:slug" element={<DetailPage />} />
         </Route>
       </Routes>
     </Router>
