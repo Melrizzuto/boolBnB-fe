774627b2e6857c54dafa9e4d6d20c0bd864ba4b4
Melrizzuto <melrizzuto4@gmail.com>
2025-02-13 16:51:42 +0100

    added  header jsx and style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+import { FaSearch } from "react-icons/fa";
+
 function Header() {
+    const [scrolled, setScrolled] = useState(false);
+
+    // Effetto cambio colore in base allo scroll
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+            setScrolled(true);
+        } else {
+            setScrolled(false);
+        }
+    });
+
     return (
-        <header className="debug">
-            <h1>Header</h1>
+        <header className={`${styles.header} ${scrolled ? styles.scrolled : ""}`}>
+            <div className={styles.container}>
+                {/* LOGO */}
+                <Link to="/" className={styles.logo}>
+                    BOOLBNB
+                </Link>
+
+                {/* NAVBAR */}
+                <nav className={styles.nav}>
+
+                </nav>
+
+                {/* PULSANTE RICERCA AVANZATA */}
+                <Link to="/search" className={styles.searchButton}>
+                    <FaSearch /> Search your fav
+                </Link>
+                {/* <Link to="/search" className={styles.searchButton}>
+                    <FaAd /> add news
+                </Link> */}
+            </div>
         </header>
     );
 }
